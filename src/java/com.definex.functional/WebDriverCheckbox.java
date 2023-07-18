96935198aa745d7fe9c627270b351189a1dfbbc3
ertugrulsen <46446809+ertugrulsen@users.noreply.github.com>
2023-07-18 18:58:04 +0300

    added chrome driver options

--- src/java/com.definex.functional/WebDriverCheckbox.java
+++ src/java/com.definex.functional/WebDriverCheckbox.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebDriverCheckbox {
     public static void main(String args[]) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\ErtugrulSen\\Desktop\\DFX\\DFX Testing Training-2022\\training-functional-test\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions opt = new ChromeOptions();
+        opt.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(opt);
         driver.get("C:\\Users\\ErtugrulSen\\Desktop\\test-training folder\\training-functional-test\\src\\java\\webapplication\\Checkbox.html");
         WebElement checkbox = driver.findElement(By.id("lettuceCheckbox"));
 
